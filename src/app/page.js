6e6b355cc5e61bdcfcdc16a816a9d2dd926d6514
“CaitlinYoung7” <caitlincnyoung@gmail.com>
2025-02-25 17:45:07 +0000

    Parralax scrolling, card stacking and smooth scroll working

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,43 @@
 import styles from "./page.module.css";
 import { projects } from "../data";
 import Card from "../components/Card";
+import { motion, useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Lenis from "lenis";
 
 export default function Home() {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+
+    // Listen for the scroll event and log the event data
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+  });
+
   return (
-    <main className={styles.main}>
-      {projects.map((project, index) => {
-        return <Card key={index} {...project} />;
+    <main ref={container} className={styles.main}>
+      {projects.map((project, i) => {
+        const targetScale = 1 - (projects.length - i) * 0.05;
+        return (
+          <Card
+            key={i}
+            i={i}
+            {...project}
+            range={[i * 0.25, 1]}
+            progress={scrollYProgress}
+            targetScale={targetScale}
+          />
+        );
       })}
     </main>
   );
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,51 @@
+"use client";
+import { useRef } from "react";
 import styles from "./style.module.css";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { M_PLUS_1 } from "next/font/google";
 
-function Index({ title, description, src, link, color }) {
+function Index({
+  i,
+  title,
+  description,
+  src,
+  link,
+  color,
+  progress,
+  range,
+  targetScale,
+}) {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "start start"],
+  });
+
+  const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+  const scale = useTransform(progress, range, [1, targetScale]);
   return (
-    <div className={styles.cardContainer}>
-      <div style={{ backgroundColor: color }} className={styles.card}>
+    <div ref={container} className={styles.cardContainer}>
+      <motion.div
+        style={{
+          scale,
+          backgroundColor: color,
+          top: `calc(-5vh + ${i * 50}px)`,
+        }}
+        className={styles.card}
+      >
         <h2 className={styles.title}>{title}</h2>
-        {/* <div className={styles.body}> */}
-        <div className={styles.description}>
-          <p>{description}</p>
+        <div className={styles.bodyContainer}>
+          <div className={styles.description}>
+            <p>{description}</p>
+          </div>
+          <div className={styles.imageContainer}>
+            <motion.div style={{ scale: imageScale }} className={styles.inner}>
+              <Image fill src={`/images/${src}`} alt="image" />
+            </motion.div>
+          </div>
         </div>
-        {/* </div> */}
-      </div>
+      </motion.div>
     </div>
   );
 }
